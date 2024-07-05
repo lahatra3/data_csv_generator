181f70ded2f0867e9aade2370ee847fc2465dd03
lahatra3 <lahatrap20.aps2a@gmail.com>
2024-07-06 00:07:16 +0300

    fix error and improving code...

--- main.mjs
+++ main.mjs
@@ -7,19 +7,27 @@ import { randomUUID, randomInt } from "node:crypto";
 
 const DATA_SINK = process.env["DATA_SINK"];
 const DATA_SINK_SIZE = parseInt(process.env["DATA_SINK_SIZE"]) || 1024;
-
 const numCPUs = availableParallelism();
+let successfulExits = 0;
+
 
 const data_writer = (filename) => {
-    const writer = createWriteStream(filename, { flags: "a", encoding: "utf-8", autoClose: true });
+    const writer = createWriteStream(filename, { 
+        flags: "a", 
+        encoding: "utf-8", 
+        autoClose: true,
+        highWaterMark: 1024 * 1024 * 731 
+    });
+
     writer.on('finish', () => {
         console.log("Writing finished...");
     });
 
     writer.on('error', (err) => {
         console.log("Error: ", err);
+        process.exit(1);
     });
-    
+
     const data1 = randomUUID().split("-").join(",");
     const data2 = randomUUID().split("-").reverse().join(",");
     const data3 = randomUUID().split("-").join(",");
@@ -30,8 +38,7 @@ const data_writer = (filename) => {
 }
 
 const fileWatcher = (filename, maxSize) => {
-
-    stat(filename, (err, stats) =>{
+    stat(filename, (err, stats) => {
         if (err) {
             console.error("Error: ", err);
             process.exit(1);
@@ -44,22 +51,33 @@ const fileWatcher = (filename, maxSize) => {
     })
 }
 
-setInterval(() => {
-
-    fileWatcher(DATA_SINK, DATA_SINK_SIZE);
-
-    if (cluster.isPrimary) {
-        console.log(`Primary PID: ${process.pid} is running ...`);
-        
-        for (let index = 0; index < numCPUs; index++) {
-           cluster.fork();
-        }
+if (cluster.isPrimary) {
+    console.log(`Primary PID: ${process.pid} is running ...`);
     
-        cluster.on('exit', (worker, code, signal) => {
-            console.log(`Worker with PID: ${worker.process.pid} is died with status code ${code} and signal ${signal} ...`);
-        });
-    } else {
-        console.log(`Worker PID: ${process.pid} is started ...`);
-        data_writer(DATA_SINK);
+    for (let index = 0; index < numCPUs; index++) {
+        cluster.fork();
     }
-}, 3331);
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker with PID: ${worker.process.pid} is died with status code ${code} and signal ${signal} ...`);
+        
+        if (code === 0) {
+            successfulExits++;
+            if (successfulExits === numCPUs) {
+                console.log('All workers have completed their tasks. Exiting primary process ...');
+                process.exit(0);
+            }
+        } else {
+            console.log("Restarting new worker...");
+            cluster.fork();
+        }
+    });
+    setInterval(() => {
+        fileWatcher(DATA_SINK, DATA_SINK_SIZE);
+    }, 1131);
+} else {
+    console.log(`Worker PID: ${process.pid} is started ...`);
+    setInterval(() => {
+        data_writer(DATA_SINK);
+    }, 731);
+}
